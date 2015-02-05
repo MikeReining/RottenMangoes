f559706b211d5caf705c08c3cedbebbe4187d1fc
Mike Reining <emailmike@gmail.com>
2015-02-05 13:39:55 -0800

    MapViewController - now grabs user location and ZIP

--- RottenMangoes/MapViewController.swift
+++ RottenMangoes/MapViewController.swift
@@ -40,5 +40,67 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         mapView.showsUserLocation = true
     }
     
+    //MARK: Location Manager Delegate
     
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        println("New location")
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        //1 grab current location
+        var currentLocation = locations.last as CLLocation
+        //2 create location point for map based on current location
+        var location = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        //3 define layout for map
+        let span = MKCoordinateSpanMake(0.02, 0.02)
+        let userPoint = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(userPoint, animated: true)
+        
+        //4 add annotation to current location
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Where am I?"
+        annotation.subtitle = "I am here!"
+        mapView.addAnnotation(annotation)
+        //5 stop updatinglocation
+        locationManager.stopUpdatingLocation()
+        
+        
+        // Reverse GEOCode
+        reverseGEOCodeFromCurrentLocation(manager)
+        
+    }
+    
+    // Reverse GeoCode Function to extract address from current Location
+    func reverseGEOCodeFromCurrentLocation(manager: CLLocationManager) {
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil) {
+                println(error.localizedDescription)
+                return
+            }
+            
+            if placemarks.count > 0 {
+                let pm = placemarks[0] as CLPlacemark
+                self.displayLocationInfo(pm)
+            } else {
+                println("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    // Print location details for GEO Lookup
+    
+    func displayLocationInfo(placemark: CLPlacemark) {
+        if placemark.postalCode != nil {
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            println(placemark.locality)
+            println(placemark.postalCode)
+            println(placemark.administrativeArea)
+            println(placemark.country)
+        }
+    }
+
 }
